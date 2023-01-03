5ec7a487d871a223ba6e3c4e3c987de184dd47cf
iaregoosedev <goosedeveloping@gmail.com>
2023-01-03 15:10:56 -0600

    Pre Publish

---
+++ src/Components/header.js
@@ -0,0 +1,11 @@
+import "./../Styles/header.css"
+
+export default function App() {
+    return (
+        <div>
+            <h1 class="test">Site not yet published.</h1>
+            <p>You can view my Figma designs for this website <a class="link" href="https://www.figma.com/file/8AeSRTABrmWvzMmeASMDAl/goose-portfolio?node-id=1%3A2&t=8DTXVc0kJQ8ZrLHs-1" target="_blank">here</a>.</p>
+            <p>You can also view my old website <a class="link" href="https://iaregoosedev.github.io/bessett-portfolio/" target="_blank">here</a>.</p>
+        </div>
+    )
+}
