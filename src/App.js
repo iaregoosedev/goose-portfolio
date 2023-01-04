d2909f265e83119181f19aa050175e183ef92a53
iaregoosedev <goosedeveloping@gmail.com>
2023-01-04 07:47:53 -0600

    Added font

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Prepublish from './Components/prepublish.js'
+import Prepublish from './Components/prepublish.js';
 
 function App() {
   return (
