7cc8257e490b836183211408b68a932a4d636830
iaregoosedev <goosedeveloping@gmail.com>
2023-02-04 15:13:38 -0600

    nav Update and favicon

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -2,6 +2,7 @@ import Logo from "../../Data/logo";
 import { Link } from "react-router-dom"
 import {Link as LinkS} from "react-scroll"
 import { useRef } from "react";
+import { useState } from "react";
 import "./navbar.css"
 
 import { Squash as Hamburger } from 'hamburger-react'
@@ -21,6 +22,14 @@ export default function Navbar() {
         headerRef.current.classList.remove("header_height");
         linkRef.current.classList.remove("visible_links");
     }
+
+    const [isOpen, setOpen] = useState(false)
+
+    const hamClick = () => {
+        if(setOpen(false)) {
+            setOpen(!isOpen);
+        }
+    }
     
     return (
         <header ref={headerRef}>
@@ -28,42 +37,36 @@ export default function Navbar() {
                 <div className="navContainer">
                     <div className="col-10 col-lg-4 navLeft">
                         {/* <Logo/> */}
-                        <LinkS className="noActive" to="hero" spy={true} smooth={true} offset={-80} duration={100} onClick={closeNav}>
+                        <LinkS className="noActive" to="hero" spy={true} smooth={true} offset={-80} duration={100} onClick={() => {closeNav(); hamClick();}}>
                             <Link to={"/"}>
                                 <h1 className="m-0">Jason Bessett</h1>
                             </Link>
                         </LinkS>
                     </div>
                     <nav ref={linkRef} className="col-lg-8">
-                        <LinkS className="navItem" to="aboutMe" spy={true} smooth={true} duration={100} onClick={showNav}>
+                        <LinkS className="navItem" to="aboutMe" spy={true} smooth={true} duration={100} onClick={() => {showNav(); hamClick();}}>
                             <li>
                                 <h5 className="m-0">About Me</h5>
                             </li>
                         </LinkS>
-                        <LinkS className="navItem" to="portfolioGallery" spy={true} smooth={true} duration={100} onClick={showNav}>
+                        <LinkS className="navItem" to="portfolioGallery" spy={true} smooth={true} duration={100} onClick={() => {showNav(); hamClick();}}>
                             <li>
                                 <h5 className="m-0">Portfolio</h5>
                             </li>
                         </LinkS>
-                        <LinkS className="navItem" to="resume" spy={true} smooth={true} duration={100} onClick={showNav}>
+                        <LinkS className="navItem" to="resume" spy={true} smooth={true} duration={100} onClick={() => {showNav(); hamClick();}}>
                             <li>
                                 <h5 className="m-0">Resume</h5>
                             </li>
                         </LinkS>
-                        <LinkS className="navItem" to="contactMe" spy={true} smooth={true} duration={100} onClick={showNav}>
+                        <LinkS className="navItem" to="contactMe" spy={true} smooth={true} duration={100} onClick={() => {showNav(); hamClick();}}>
                             <li>
                                 <h5 className="m-0">Contact Me</h5>
                             </li>
                         </LinkS>
                     </nav>
                     <div className="navButtin" onClick={showNav}>
-                        {/* <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                        </svg> */}
-                        <Hamburger rounded size={30}/>
+                        <Hamburger toggled={isOpen} toggle={setOpen} rounded size={30}/>
                     </div>
                 </div>
             </div>
