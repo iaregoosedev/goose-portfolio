a4b91b9aaaf0954bf8c9f0b22f3deeebbf0b68df
iaregoosedev <goosedeveloping@gmail.com>
2023-02-09 11:25:18 -0600

    added footer

--- src/Pages/OSFPage/osfPage.js
+++ src/Pages/OSFPage/osfPage.js
@@ -3,6 +3,7 @@ import ScrollToTop from "../../Components/ScrollToTop";
 import OsfHero from "../../Components/OSFHero/osfHero";
 import OSFDesigns from "../../Components/OSFDesignCarousel/osfDesigns";
 import Navbar from "../../Components/Navbar/navbar";
+import Footer from "../../Components/Footer/footer";
 
 export default function OSFPage() {
     return (
@@ -11,6 +12,7 @@ export default function OSFPage() {
             <Navbar state="none" pageName="OSF"/> 
             <OsfHero/>
             <OSFDesigns/>
+            <Footer/>
         </div>
     )
 }
