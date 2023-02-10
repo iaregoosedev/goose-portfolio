f0108555ac85dcf3e8db586a687656b90d73eb54
iaregoosedev <goosedeveloping@gmail.com>
2023-02-10 15:38:03 -0600

    Updated Resume

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -14,7 +14,7 @@ export default function Resume() {
                 <div className="col-lg-6 aboutLeft pe-0">
                     <h6 className="resMob" style={{color: '#dd9510'}}>RESUME</h6>
                     <h2>Please feel free to download my resume</h2>
-                    <a href={require('../../Data/Bessett_Resume_2023.pdf')} download="Bessett_Resume"><Buttin>Download</Buttin></a>
+                    <a href={require('../../Data/bessettJason_resume_2023.pdf')} download="Bessett_Resume"><Buttin>Download and View</Buttin></a>
                 </div>
             </div>
         </div>
