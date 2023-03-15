26443f35120ebc41ff822553336e68eaa65b9621
iaregoosedev <goosedeveloping@gmail.com>
2023-03-15 12:25:37 -0500

    Resume Update

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,20 +1,20 @@
 import "../Resume/Resume.css"
 import React from "react"
 import Buttin from "../Buttin/buttin.js"
-import ResumeImg from "../../Images/Resume_2023.png"
+import ResumeImg from "../../Images/Resume_v2.png"
 
 export default function Resume() {
     return (
         <div className="resume">
-            <div className="resumeContainer row gx-6">
+            <div className="resumeContainer row p-0 gx-6">
                 <div className="col-lg-6 aboutRight ps-0">
                     <h6 className="resDesk" style={{color: '#dd9510'}}>RESUME</h6>
-                    <img alt="Resume Image" className="resumeImg" src={require('../../Images/Resume_2023.png')}/>
+                    <img alt="Resume Image" className="resumeImg" src={require('../../Images/Resume_v2.png')}/>
                 </div>
                 <div className="col-lg-6 aboutLeft pe-0">
                     <h6 className="resMob" style={{color: '#dd9510'}}>RESUME</h6>
                     <h2>Please feel free to download my resume</h2>
-                    <a href={require('../../Data/bessettJason_resume_2023.pdf')} download="Bessett_Resume"><Buttin>Download and View</Buttin></a>
+                    <a href={require('../../Data/Resume_v2.pdf')} download="Bessett_Resume"><Buttin>Download and View</Buttin></a>
                 </div>
             </div>
         </div>
