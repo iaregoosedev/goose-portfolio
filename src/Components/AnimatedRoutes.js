1b01d5c319cce6b7d8fa953af7aa34ad1e73882e
iaregoosedev <jayzgoose25@gmail.com>
2024-07-03 12:24:05 -0500

    Added Gooselog

--- src/Components/AnimatedRoutes.js
+++ src/Components/AnimatedRoutes.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Homepage from '../Pages/HomePage/homepage';
-import OSFPage from '../Pages/OSFPage/osfPage';
-import {AnimatePresence} from "framer-motion";
-import SSPage from '../Pages/SSPage/ssPage';
-import CATPage from '../Pages/CATPage/catPage';
-import LLPage from '../Pages/LLPage/llPage';
-import OPage from '../Pages/OPage/oPage';
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Homepage from "../Pages/HomePage/homepage";
+import OSFPage from "../Pages/OSFPage/osfPage";
+import { AnimatePresence } from "framer-motion";
+import SSPage from "../Pages/SSPage/ssPage";
+import CATPage from "../Pages/CATPage/catPage";
+import LLPage from "../Pages/LLPage/llPage";
+import OPage from "../Pages/OPage/oPage";
+import Gamelog from "../Pages/Gamelog/gamelog";
 
 function AnimatedRoutes() {
-    const location = useLocation();
+  const location = useLocation();
   return (
-    <AnimatePresence mode='wait'>
-        <Routes location={location} key={location.pathname}>
-            <Route path='/' element={<Homepage/>}/>
-            <Route path='/OLIMP' element={<OPage/>}/>
-            <Route path='/LuminosLabs' element={<LLPage/>}/>
-            <Route path='/OSF' element={<OSFPage/>}/>
-            <Route path='/CAT' element={<CATPage/>}/>
-            <Route path='/7Summits' element={<SSPage/>}/>
-        </Routes>
-    </AnimatePresence> 
-  )
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/OLIMP" element={<OPage />} />
+        <Route path="/LuminosLabs" element={<LLPage />} />
+        <Route path="/OSF" element={<OSFPage />} />
+        <Route path="/CAT" element={<CATPage />} />
+        <Route path="/7Summits" element={<SSPage />} />
+        <Route path="/gamelog" element={<Gamelog />} />
+      </Routes>
+    </AnimatePresence>
+  );
 }
 
-export default AnimatedRoutes
+export default AnimatedRoutes;
---
+++ src/Pages/Gamelog/gamelog.js
@@ -0,0 +1,30 @@
+import "../Gamelog/gamelog.css";
+import { motion as m } from "framer-motion";
+import Navbar from "../../Components/Navbar/navbar";
+import Footer from "../../Components/Footer/footer";
+
+export default function Gamelog() {
+  return (
+    <div className="gamelog">
+      <Navbar />
+      <div className="mondayContainer">
+        <iframe
+          className="mondayIframe"
+          src='https://view.monday.com/embed/6897085069-fca2d12bad496c1c5dcc53ee9ffdf92a?r=use1" width=770 height=500 style="overflow-x: hidden; border: 0; box-shadow: 5px 5px 56px 0px rgba(0,0,0,0.25);'
+        />
+      </div>
+      <Footer />
+      <m.div
+        className="slider"
+        // initial={{transform: "translateY(100vh)"}}
+        animate={{
+          transform: "translateY(100vh)",
+          transition: { duration: 0.3 },
+        }}
+        exit={{ transform: "translateY(0vh)", transition: { duration: 0.3 } }}
+      >
+        <div className="slideImg" id="slideBg"></div>
+      </m.div>
+    </div>
+  );
+}
