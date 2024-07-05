c16591e72fa222515629db5e24c5ea651b709a7d
iaregoosedev <jayzgoose25@gmail.com>
2024-07-05 11:50:00 -0500

    change max width from 1200 to 1400

--- src/Pages/OPage/oPage.js
+++ src/Pages/OPage/oPage.js
@@ -1,19 +1,30 @@
-import "../SSPage/ssPage.css"
+import "../SSPage/ssPage.css";
 import ScrollToTop from "../../Components/ScrollToTop";
-import Footer from "../../Components/Footer/footer"
+import Footer from "../../Components/Footer/footer";
 import LLHero from "../../Components/LLHero/llHero";
 import Navbar from "../../Components/Navbar/navbar";
 
 export default function OPage() {
-    return (
-        <div className="ssPage">
-            <ScrollToTop/> 
-            <Navbar state="none" pageName="OLIMP"/>
-            <div style={{display: 'flex', flexDirection:'column',height: 'calc(100vh - 364px)', display:'flex', alignItems:'center', justifyContent:'center', paddingTop: '124px'}}>
-               <img alt='Goose Gif' src={require('../../Images/goose.gif')}></img>
-               Coming Soon... 
-            </div>
-            <Footer/>
-        </div>
-    )
+  return (
+    <div className="ssPage">
+      <ScrollToTop />
+      <Navbar state="none" pageName="OLIMP" />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "520px",
+          height: "calc(100vh - 364px)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          paddingTop: "124px",
+        }}
+      >
+        <img alt="Goose Gif" src={require("../../Images/goose.gif")}></img>
+        Coming Soon...
+      </div>
+      <Footer />
+    </div>
+  );
 }
